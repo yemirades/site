47ca0195aa831a45f082e70f03e23710e55127f2
Mirat Yerbolat <yemirades@users.noreply.github.com>
2026-07-28 09:45:07 +0500

    Redesign client logo grid from Figma

--- src/components/Clients.tsx
+++ src/components/Clients.tsx
@@ -7,55 +7,87 @@ const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
 
 const clients = [
   {
-    name: "BI Group",
-    src: "client-bi-group.svg",
-    width: 200,
-    height: 51,
-    size: "w-[110px] sm:w-[132px]",
-  },
-  {
     name: "Mycar",
     src: "client-mycar.svg",
     width: 136,
     height: 49,
-    size: "w-[78px] sm:w-[96px]",
+    size: "w-[82px] sm:w-[100px] lg:w-[124px]",
   },
   {
     name: "Astana Motors",
     src: "client-astana-motors.svg",
     width: 144,
     height: 46,
-    size: "w-[86px] sm:w-[108px]",
+    size: "w-[94px] sm:w-[116px] lg:w-[142px]",
   },
   {
-    name: "SDL School",
-    src: "client-sdl-school.svg",
-    width: 146,
-    height: 43,
-    size: "w-[92px] sm:w-[112px]",
+    name: "BI Group",
+    src: "client-bi-group.svg",
+    width: 200,
+    height: 51,
+    size: "w-[112px] sm:w-[136px] lg:w-[156px]",
   },
   {
     name: "Client logo",
     src: "client-round-mark.png",
     width: 79,
     height: 79,
-    size: "w-[36px] sm:w-[44px]",
+    size: "w-[42px] sm:w-[50px] lg:w-[62px]",
+  },
+  {
+    name: "Daryn Online",
+    src: "client-daryn-online.svg",
+    width: 160,
+    height: 54,
+    size: "w-[112px] sm:w-[142px] lg:w-[170px]",
   },
   {
     name: "Mūğalim",
     src: "client-mugalim.svg",
     width: 246,
     height: 57,
-    size: "w-[116px] sm:w-[146px]",
+    size: "w-[116px] sm:w-[144px] lg:w-[174px]",
+  },
+  {
+    name: "SDL School",
+    src: "client-sdl-school.svg",
+    width: 146,
+    height: 43,
+    size: "w-[96px] sm:w-[116px] lg:w-[138px]",
   },
   {
     name: "Client symbol",
     src: "client-symbol.svg",
     width: 58,
     height: 57,
-    size: "w-[30px] sm:w-[38px]",
+    size: "w-[32px] sm:w-[42px] lg:w-[54px]",
   },
 ];
+
+function CornerMarks() {
+  const cornerClass = "absolute size-3 border-white/20";
+
+  return (
+    <>
+      <span
+        aria-hidden="true"
+        className={`${cornerClass} left-0 top-0 border-l border-t`}
+      />
+      <span
+        aria-hidden="true"
+        className={`${cornerClass} right-0 top-0 border-r border-t`}
+      />
+      <span
+        aria-hidden="true"
+        className={`${cornerClass} bottom-0 left-0 border-b border-l`}
+      />
+      <span
+        aria-hidden="true"
+        className={`${cornerClass} bottom-0 right-0 border-b border-r`}
+      />
+    </>
+  );
+}
 
 export function Clients() {
   const { lang } = useLang();
@@ -65,7 +97,7 @@ export function Clients() {
       aria-labelledby="clients-title"
       className="theme-surface overflow-hidden py-16 sm:py-20"
     >
-      <div className="mx-auto max-w-[1200px] px-4 sm:px-8 lg:max-w-none lg:px-6">
+      <div className="mx-auto max-w-[1440px] px-3 sm:px-8 lg:px-6">
         <h2
           id="clients-title"
           className="max-w-[760px] font-display text-[42px] font-normal leading-[0.9] tracking-[-0.03em] sm:text-[64px]"
@@ -82,28 +114,29 @@ export function Clients() {
             </>
           )}
         </h2>
-      </div>
 
-      <div
-        className="mx-auto mt-12 grid max-w-[1200px] grid-cols-2 gap-3 px-3 sm:mt-14 sm:px-8 lg:grid-cols-4 lg:gap-5 lg:px-6"
-        aria-label="Client logos"
-      >
-        {clients.map((client) => (
-          <div
-            key={client.src}
-            className="flex h-[104px] items-center justify-center bg-[#101010] sm:h-[132px]"
-          >
-            <div className={client.size}>
-              <Image
-                src={`${basePath}/${client.src}`}
-                alt={client.name}
-                width={client.width}
-                height={client.height}
-                className="h-auto max-h-11 w-full object-contain sm:max-h-[52px]"
-              />
+        <div
+          className="mt-12 grid grid-cols-2 sm:mt-14 lg:grid-cols-4"
+          aria-label="Client logos"
+        >
+          {clients.map((client) => (
+            <div
+              key={client.src}
+              className="relative flex h-[132px] items-center justify-center sm:h-[168px] lg:h-[220px]"
+            >
+              <CornerMarks />
+              <div className={client.size}>
+                <Image
+                  src={`${basePath}/${client.src}`}
+                  alt={client.name}
+                  width={client.width}
+                  height={client.height}
+                  className="h-auto w-full object-contain"
+                />
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </section>
   );
