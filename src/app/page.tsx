d4766ffe430fce10a5f38752f62603044c51533e
yemirades <yerbolatov.mirat@gmail.com>
2026-08-19 17:43:27 +0500

    Adopt editorial portfolio index layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,11 @@
-import { Nav } from "@/components/Nav";
-import { CursorTrail } from "@/components/CursorTrail";
-import { Hero } from "@/components/Hero";
-import { Works } from "@/components/Works";
-import { Approach } from "@/components/Approach";
-import { Services } from "@/components/Services";
-import { Contacts } from "@/components/Contacts";
 import { PageLoader } from "@/components/PageLoader";
+import { PortfolioIndex } from "@/components/PortfolioIndex";
 
 export default function Home() {
   return (
     <>
       <PageLoader />
-      <CursorTrail />
-      <Nav />
-      <main>
-        <Hero />
-        <Works />
-        <Approach />
-        <Services />
-        <Contacts />
-      </main>
+      <PortfolioIndex />
     </>
   );
 }
